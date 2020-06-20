8dbf24f7a587dfc3ec074213712c1eb1c34100be
morgan smith <mssmitty07@gmail.com>
2020-06-19 22:35:17 -0400

    created folders

---
+++ AnytimeFitness/AnytimeFitness/Views/View Controllers/Client/Features/Search/LocationSearchViewController.swift
@@ -0,0 +1,19 @@
+//
+//  LocationSearchViewController.swift
+//  AnytimeFitness
+//
+//  Created by Morgan Smith on 6/19/20.
+//  Copyright © 2020 Jarren Campos. All rights reserved.
+//
+
+import UIKit
+
+class LocationSearchViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
