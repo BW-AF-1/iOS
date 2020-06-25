c887e3d720a1b6512b5985326e33c73ea452dfc8
Jarren <jarrenmcampos@gmail.com>
2020-06-24 21:08:35 -0700

    Working master

--- AnytimeFitness/AnytimeFitness/Controllers/UserController.swift
+++
@@ -1,9 +0,0 @@
-//
-//  UserController.swift
-//  AnytimeFitness
-//
-//  Created by Jarren Campos on 6/24/20.
-//  Copyright © 2020 Jarren Campos. All rights reserved.
-//
-
-import Foundation
--- AnytimeFitness/Resources/HTTPMethod.swift
+++ AnytimeFitness/Resources/HTTPMethod.swift
@@ -7,3 +7,10 @@
 //
 
 import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case put = "PUT"
+    case post = "POST"
+    case delete = "DELETE"
+}
